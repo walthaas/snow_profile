ebdf3b412cfe6ff3bc99e9258b90a77e5511d381
Walter Haas <haas@xmission.com>
2013-10-18 17:48:59 -0600

    depth movement working

--- snow_profile.js
+++ snow_profile.js
@@ -53,12 +53,6 @@
   var GRAPH_CENTER_X = DEPTH_LABEL_WD + 1 + (GRAPH_WIDTH / 2);
 
   /**
-   * Central y of the data plotting area
-   * @const
-   */
-  var GRAPH_CENTER_Y = (GRAPH_HEIGHT / 2);
-
-  /**
    * Maximum x value allowed for a handle (hardness 'I').
    * @const
    */
@@ -129,13 +123,28 @@
   var snow_profile_stage;
 
   /**
+   * Maximum snow depth in cm that can be plotted on the graph
+   *
+   * Snow depth in cm that corresponds to GRAPH_HEIGHT pixels.
+   * Zero depth always corresponds to zero graph pixels.
+   */
+  var snow_profile_max_depth = 200;
+
+  /**
    * Object to manage a doubly-linked list
    *
    * The objects on the list have properties 'before' and 'after' pointing
    * to the object before resp. after on the list.
+   * @class
+   * @constructs Dlist
    */
   function Dlist() {
-    // The first object in the list
+
+    "use strict";
+    /**
+     * The first object in the list.
+     * @member {Object}
+     */
     this.first = null;
 
     // The last object in the list
@@ -145,15 +154,16 @@
   // Insert an object before a member of a list
   Dlist.prototype.insertBefore = function(member, beforeThis) {
       // FIXME
-    }
+  };
 
   // Insert an object after a member of a list
   Dlist.prototype.insertAfter = function(member, afterThis) {
       // FIXME
-  }
+  };
 
   // Add an object to the tail of the list
   Dlist.prototype.append = function(member) {
+    "use strict";
     if (this.first === null) {
 
       // The list is now empty so this object is both first and last.
@@ -163,16 +173,16 @@
 
       // The list is not empty.  Add this object to the tail of the list.
       var oldTail = this.tail;
-      this.tail = oldTail.after = member;
+      this.tail = member;
       member.before = oldTail;
       member.after = null;
     }
-  }
+  };
 
   // Delete an object from the list
   Dlist.prototype.delete = function(member) {
       // FIXME
-  }
+  };
 
   /**
    * Snow stratigraphy snow layers
@@ -186,11 +196,13 @@
 
   /**
    * Object describing a single snow stratigraphy layer
+   * @constructor
    */
   function SnowProfileLayer(depth, hardness) {
+    "use strict";
 
     // Reference this object inside an event handler
-    thisObj = this;
+    var thisObj = this;
 
     // The layer object before (above) this one.
     // Points to a SnowProfileLayer or is null.
@@ -211,8 +223,8 @@
     // Handle for the line at the top of the layer.
     // The user drags and drops this handle to adjust depth and hardness.
     this.handle = new Kinetic.Rect({
-      x: GRAPH_CENTER_X,
-      y: 0,
+      x: thisObj.code2x(thisObj.hardness),
+      y: thisObj.depth2y(thisObj.depth),
       width: HANDLE_SIZE,
       height: HANDLE_SIZE,
       fill: '#000',
@@ -225,17 +237,42 @@
         else if (pos.x > HANDLE_MAX_X) {
           newX = HANDLE_MAX_X;
         }
+        var newY = pos.y;
+        if (thisObj.before === null) {
+
+          // This is the top (snow surface) layer.
+          // Handle stays on the surface.
+          newY = 0;
+        }
+        else if (thisObj.after === null) {
+
+          // This is the bottom layer.  The handle depth is constrained
+          // between the layer above and GRAPH_HEIGHT.
+          if (pos.y > (GRAPH_HEIGHT - HANDLE_SIZE)) {
+            newY = GRAPH_HEIGHT - HANDLE_SIZE;
+          }
+          else if (pos.y < 1) {
+            newY = 1;
+          }
+        }
+        else {
+
+          // This layer is below the surface and above the bottom.
+          // The handle depth is contstrained between layers above and below.
+          // FIXME          
+        }
+        thisObj.depth = thisObj.y2depth(newY);
         return{
           x: newX,
-          y: 0
-        }
+          y: newY
+        };
       }
     });
 
     // Add text to show current surface handle location.
     this.handle_loc = new Kinetic.Text({
       x: DEPTH_LABEL_WD + 1 + GRAPH_WIDTH + 10,
-      y: 0,
+      y: thisObj.depth2y(thisObj.depth),
       fontSize: 12,
       fontStyle: 'bold',
       fontFamily: 'sans-serif',
@@ -248,19 +285,21 @@
     this.handle.on('mouseover', function() {
       document.body.style.cursor = 'pointer';
       thisObj.handle_loc.setVisible(1);
-      stage.draw();
+      snow_profile_stage.draw();
     });
     this.handle.on('mouseout', function() {
       document.body.style.cursor = 'default';
       thisObj.handle_loc.setVisible(0);
-      stage.draw();
+      snow_profile_stage.draw();
     });
     layer.add(this.handle);
 
     // Add a horizontal line at the top of the layer
     this.horiz_line = new Kinetic.Line({
-      points: [[DEPTH_LABEL_WD + 1, Math.floor(HANDLE_SIZE / 2)],
-               [this.handle.getX(), Math.floor(HANDLE_SIZE / 2)]],
+      points: [[DEPTH_LABEL_WD + 1,
+                thisObj.depth2y(thisObj.depth) + Math.floor(HANDLE_SIZE / 2)],
+               [this.handle.getX(),
+                thisObj.depth2y(thisObj.depth) + Math.floor(HANDLE_SIZE / 2)]],
       stroke: '#000'
     });
     layer.add(this.horiz_line);
@@ -270,25 +309,77 @@
 
     // When the handle moves, recalculate the hardness value displayed
     this.handle.on('dragmove', function() {
-      thisObj.horiz_line.setPoints([DEPTH_LABEL_WD + 1, 3, thisObj.handle.getX(), 3]);
-      var hard_code = 'I';
-      for (var i = 0; i < CAAML_HARD.length - 1; i++) {
-        if ((thisObj.handle.getX() >= (CAAML_HARD[i][2])
-            && (thisObj.handle.getX() < (CAAML_HARD[i+1][2])))) {
-          hard_code = CAAML_HARD[i][0];
-          break;
-        }
-      }
-      thisObj.handle_loc.setText( '(0,' + hard_code + ')');
+      thisObj.horiz_line.setPoints([
+        [DEPTH_LABEL_WD + 1,
+          thisObj.depth2y(thisObj.depth) + Math.floor(HANDLE_SIZE / 2)],
+        [thisObj.handle.getX(),
+         thisObj.depth2y(thisObj.depth) + Math.floor(HANDLE_SIZE / 2)]
+      ]);
+      var mm = Math.round(thisObj.depth * 10) / 10;
+      thisObj.handle_loc.setText( '(' + mm + ', ' +
+        thisObj.x2code(thisObj.handle.getX()) + ')');
+      thisObj.handle_loc.setY(thisObj.depth2y(thisObj.depth));
     });
     layer.add(this.handle_loc);
   } // function SnowProfileLayer()
 
   /**
+   * Convert a hardness code to an X axis position
+   */
+  SnowProfileLayer.prototype.code2x = function(code) {
+    "use strict";
+    var x = HANDLE_MIN_X;
+    for (var i = 0; i < CAAML_HARD.length; i++) {
+      if (code === CAAML_HARD[i][0]) {
+        x = CAAML_HARD[i][2] + HARD_BAND_WD * 0.5;
+        break;
+      }
+    }
+    return x;
+  };
+
+  /**
+   * Convert an X axis position to a hardness code
+   */
+  SnowProfileLayer.prototype.x2code = function(x) {
+    "use strict";
+    var code = 'I';
+    for (var i = 0; i < CAAML_HARD.length - 1; i++) {
+      if ((x >= (CAAML_HARD[i][2]) &&
+        (x < (CAAML_HARD[i+1][2])))) {
+        code = CAAML_HARD[i][0];
+        break;
+      }
+    }
+    return code;
+  };
+
+  /**
+   * Convert a depth in cm to a Y axis position
+   *
+   * Y axis scale is max depth of snow / graph height in pixels
+   * NB: assumes snow surface at Y == 0.
+   */
+  SnowProfileLayer.prototype.depth2y = function(depth) {
+    "use strict";
+    return depth * (GRAPH_HEIGHT / snow_profile_max_depth);
+  };
+
+  /**
+   * Convert a Y axis position to a depth in cm
+   * NB: assumes snow surface at Y == 0.
+   */
+  SnowProfileLayer.prototype.y2depth = function(y) {
+    "use strict";
+    return (y / GRAPH_HEIGHT) * snow_profile_max_depth;
+  };
+
+  /**
    * Initialize the container and the grid layer
    */
   function snow_profile_init() {
-    stage = new Kinetic.Stage({
+    "use strict";
+    snow_profile_stage = new Kinetic.Stage({
       container: 'snow_profile_diagram',
       width: STAGE_WD,
       height: STAGE_HT
@@ -361,7 +452,7 @@
           fill: LABEL_COLOR,
           align: 'center'
         }));
-      };
+      }
     }
     var hardness_text = new Kinetic.Text({
       x: GRAPH_CENTER_X,
@@ -381,7 +472,12 @@
    * Main program
    */
   snow_profile_init();
-  surf_layer = new SnowProfileLayer(0, '1F');
-
+  snow_profile_layers.append(new SnowProfileLayer(0, '4F'));
+  snow_profile_layers.append(new SnowProfileLayer(25, '1F'));
   // add the layer to the stage
-  stage.add(layer);
+  snow_profile_stage.add(layer);
+
+// Configure Emacs for Drupal JavaScript coding standards
+// Local variables:
+// mode: js2-mode
+// End:
