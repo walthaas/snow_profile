596678f44d756a44ea6e18888e2aebff5d928587
Walter Haas <haas@xmission.com>
2014-06-20 15:59:40 -0600

    Add QUnit tests. Clean up Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,29 @@
 module.exports = function(grunt) {
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-qunit');
 
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        jsdoc: {
+            src: [ 'README.md', '<%= srcs %>' ],
+            options:{
+                configure: 'jsdoc.json',
+                destination: './doc'
+            }
+        },
         jshint: {
             options: {
                 jshintrc: true
             },
-            all: ["snow_profile*.js"]
-        }
+            all: [ '<%= srcs %>' ]
+        },
+        srcs: [ "snow_profile*.js", "!snow_profile.min.js" ]
     });
 
+    // Load plugins
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-jsdoc');
+
+    // Task aliases
+    grunt.registerTask('default', ['jshint']);
+    grunt.registerTask('doc',     ['jsdoc']);
 };
