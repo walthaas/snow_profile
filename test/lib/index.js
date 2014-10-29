49849706ac46d68d909ea1e80ce05adbfa24f271
Walter Haas <haas@xmission.com>
2014-10-29 08:07:32 -0600

    finish refactoring setFeaturesComment

--- test/lib/index.js
+++ test/lib/index.js
@@ -10,42 +10,227 @@ exports.testURL = 'file://' + process.cwd() + '/test/lib/test.html';
  * Store comment into popup comment field.
  */
 function setFeaturesComment(sw, driver, comment) {
-  var cmdStrComment = [],
-    commentStarted = false,
-    commentDone = false;
 
+  var cmdStr = [],
+    commentDone = false,
+    i;
+
+  if ((comment === null) || (comment === undefined)) {
+    return;
+  }
+
+  // If there is existing comment text, erase it
+  driver.executeScript('return $("#snow_profile_comment").val()')
+    .then(function(val) {
+      if (val.length > 0) {
+	for (i = 0; i < val.length; i++) {
+	  cmdStr.push(sw.Key.BACK_SPACE);
+	}
+	cmdStr.push(sw.Key.NULL);
+      }
+    });
+
+  // After backspacing over existing contents of input box,
+  // type in the comment
+  driver.findElement(sw.By.css('#snow_profile_comment'))
+    .then(function(elmt) {
+      cmdStr.push(comment);
+      elmt.sendKeys.apply(elmt, cmdStr);
+    });
+
+  // Wait until new text is stored
   driver.wait(function() {
-    if (!commentStarted) {
-      commentStarted = true;
-      // If there is existing comment text, erase it
-      driver.executeScript('return $("#snow_profile_comment").val()')
-	.then(function(val) {
-	  if (val.length > 0) {
-	    for (i = 0; i < val.length; i++) {
-	      cmdStrComment.push(sw.Key.BACK_SPACE);
-	    }
-	    cmdStrComment.push(sw.Key.NULL);
-	  }
-	});
-
-      // After backspacing over existing contents of input box,
-      // type in the comment
-      driver.findElement(sw.By.css('#snow_profile_comment'))
-	.then(function(elmt) {
-	  cmdStrComment.push(comment);
-	  elmt.sendKeys.apply(elmt, cmdStrComment);
-	});
-    }
-
-    // Check whether new text is stored
     driver.executeScript('return $("#snow_profile_comment").val()')
       .then(function(val) {
 	if (val === comment) {
 	  commentDone = true;
 	}
-      });
+    });
     return commentDone;
   }, 2000, "comment wasn't stored");
+}
+
+/**
+ * Set grain shape.
+ */
+function setFeaturesShape(sw, driver, shape) {
+
+  var primaryShape,
+    primarySubShape,
+    secondaryShape,
+    secondarySubShape;
+
+  // Shape is a string specifying primary shape, or an array of two
+  // strings specifying primary or seconday shape.  May be null.
+  if ((shape === null) && (shape === undefined)) {
+    return;
+  }
+
+  // If shape info was supplied, parse the info into
+  // primary and secondary shape and subshape
+  if (Object.prototype.toString.call(shape) === '[object Array]') {
+    if (shape[0].length === 4) {
+      primaryShape = shape[0].substr(0,2);
+      primarySubShape = shape[0];
+    }
+    else {
+      primaryShape = shape[0];
+    }
+    if (shape[1].length === 4) {
+      secondaryShape = shape[1].substr(0,2);
+      secondarySubShape = shape[1];
+    }
+    else {
+      secondaryShape = shape[1];
+    }
+  }
+  else {
+    if (shape.length === 4) {
+      primaryShape = shape.substr(0,2);
+      primarySubShape = shape;
+    }
+    else {
+      primaryShape = shape;
+    }
+  }
+
+  // Store primary shape info
+  driver.findElement(sw.By.xpath(
+    '//select[@id="snow_profile_primary_grain_shape"]/option[@value="' +
+    primaryShape + '"]'))
+    .click();
+
+  // If primary subshape info found, store that
+  if (primarySubShape !== undefined) {
+    driver.wait(function() {
+      return driver.isElementPresent(sw.By.xpath(
+        '//select[@id="snow_profile_primary_grain_subshape_' + primaryShape +
+        '"]'));
+      }, 2000, 'subselect for ' + primaryShape + ' not found')
+      .then(function() {
+	driver.findElement(sw.By.xpath(
+          '//select[@id="snow_profile_primary_grain_subshape_' +
+          primaryShape + '"]/option[@value="' + primarySubShape + '"]'))
+          .click();
+      });
+  }
+
+  // If secondary shape info found, store that
+  if (secondaryShape !== undefined) {
+    driver.wait(function() {
+      return driver.isElementPresent(sw.By.xpath(
+        '//select[@id="snow_profile_secondary_grain_select"]'));
+      }, 2000, 'snow_profile_secondary_grain_select not found')
+      .then(function() {
+	driver.findElement(sw.By.xpath(
+          '//select[@id="snow_profile_secondary_grain_select"]/option[@value="'
+          + secondaryShape + '"]'))
+          .click();
+	});
+    if (secondarySubShape !== undefined) {
+      driver.wait(function() {
+	return driver.isElementPresent(sw.By.xpath(
+          '//select[@id="snow_profile_secondary_grain_subshape_' +
+          secondaryShape + '"]'));
+      }, 2000, 'snow_profile_secondary_grain_subshape select not found')
+      .then(function() {
+	driver.findElement(sw.By.xpath(
+          '//select[@id="snow_profile_secondary_grain_subshape_' +
+          secondaryShape +'"]/option[@value="' + secondarySubShape +
+          '"]'))
+          .click();
+	});
+    }
+  }
+}
+
+/**
+ * Set grain size.
+ */
+function setFeaturesSize(sw, driver, size) {
+  var cmdStrMin = [],
+    cmdStrMax = [],
+    i,
+    minDone = false,
+    maxDone = false,
+    sizeMin,
+    sizeMax;
+
+  // Size is a number specifying grain size, or an array of two
+  // numbers specifying a range of sizes.  May be null.
+  if ((size === null) || (size === undefined)) {
+    return;
+  }
+
+  if (Object.prototype.toString.call(size) === '[object Array]') {
+    sizeMin = String(size[0]);
+    sizeMax = String(size[1]);
+  }
+  else {
+    sizeMin = String(size);
+  }
+
+  // If there is existing minimum grain size, erase it
+  driver.executeScript('return $("#snow_profile_grain_size_min").val()')
+    .then(function(val) {
+      if (val.length > 0) {
+	for (i = 0; i < val.length; i++) {
+	  cmdStrMin.push(sw.Key.BACK_SPACE);
+	}
+	cmdStrMin.push(sw.Key.NULL);
+      }
+    });
+
+  // After backspacing over existing contents of input box,
+  // type in the minimum grain size
+  driver.findElement(sw.By.css('#snow_profile_grain_size_min'))
+    .then(function(elmt) {
+      cmdStrMin.push(sizeMin);
+      elmt.sendKeys.apply(elmt, cmdStrMin);
+    });
+
+  // Wait until new minimum size is stored
+  driver.wait(function() {
+    driver.executeScript('return $("#snow_profile_grain_size_min").val()')
+      .then(function(val) {
+	if (val === sizeMin) {
+	  minDone = true;
+	}
+    });
+    return minDone;
+  }, 2000, "minimum grain size wasn't stored");
+
+  if (sizeMax !== undefined) {
+    // If there is existing maximum grain size, erase it
+    driver.executeScript('return $("#snow_profile_grain_size_max").val()')
+      .then(function(val) {
+	if (val.length > 0) {
+	  for (i = 0; i < val.length; i++) {
+	    cmdStrMax.push(sw.Key.BACK_SPACE);
+	  }
+	  cmdStrMax.push(sw.Key.NULL);
+	}
+      });
+
+    // After backspacing over existing contents of input box,
+    // type in the maximum grain size
+    driver.findElement(sw.By.css('#snow_profile_grain_size_max'))
+      .then(function(elmt) {
+	cmdStrMax.push(sizeMax);
+	elmt.sendKeys.apply(elmt, cmdStrMax);
+      });
+
+    // Wait until new maximum size is stored
+    driver.wait(function() {
+      driver.executeScript('return $("#snow_profile_grain_size_max").val()')
+	.then(function(val) {
+	  if (val === sizeMax) {
+	    maxDone = true;
+	  }
+      });
+      return maxDone;
+    }, 2000, "maximum grain size wasn't stored");
+  }
 }
 
 /**
@@ -61,62 +246,8 @@ function setFeaturesComment(sw, driver, comment) {
 exports.setFeatures = function setFeatures(sw, driver, index, shape, size,
   comment) {
 
-  var cmdStrMin = [],
-    cmdStrMax = [],
-    i,
-    primaryShape,
-    primarySubShape,
-    secondaryShape,
-    secondarySubShape,
-    sizeMin,
-    sizeMax,
-    popupDisplayed;
+  var popupDisplayed;
 
-
-  // Shape is a string specifying primary shape, or an array of two
-  // strings specifying primary or seconday shape.  May be null.
-  if ((shape !== null) && (shape !== undefined)) {
-
-    // If shape info was supplied, parse the info into
-    // primary and secondary shape and subshape
-    if (Object.prototype.toString.call(shape) === '[object Array]') {
-      if (shape[0].length === 4) {
-        primaryShape = shape[0].substr(0,2);
-        primarySubShape = shape[0];
-      }
-      else {
-        primaryShape = shape[0];
-      }
-      if (shape[1].length === 4) {
-        secondaryShape = shape[1].substr(0,2);
-        secondarySubShape = shape[1];
-      }
-      else {
-        secondaryShape = shape[1];
-      }
-    }
-    else {
-      if (shape.length === 4) {
-        primaryShape = shape.substr(0,2);
-        primarySubShape = shape;
-      }
-      else {
-        primaryShape = shape;
-      }
-    }
-  }
-
-  // Size is a number specifying grain size, or an array of two
-  // numbers specifying a range of sizes.  May be null.
-  if ((size !== null) && (size !== undefined)) {
-    if (Object.prototype.toString.call(size) === '[object Array]') {
-      sizeMin = size[0];
-      sizeMax = size[1];
-    }
-    else {
-      sizeMin = size;
-    }
-  }
 
   // Click the Edit button for the layer to open the popup.
   driver.findElement(sw.By.xpath(
@@ -125,89 +256,19 @@ exports.setFeatures = function setFeatures(sw, driver, index, shape, size,
   .click();
 
   driver.wait(function() {
-    return driver.findElement(sw.By.css('div#snow_profile_popup')).isDisplayed();
+    return driver.findElement(sw.By.css('div#snow_profile_popup'))
+      .isDisplayed();
     }, 2000, 'div#snow_profile_popup not found')
     .then(function() {
-      if (primaryShape !== undefined) {
 
-        // Set popup <select>s to shape values
-        driver.findElement(sw.By.xpath('//select[@id="snow_profile_primary_grain_shape"]/option[@value="' + primaryShape + '"]')).click();
-        if (primarySubShape !== undefined) {
-          driver.wait(function() {
-            return driver.isElementPresent(sw.By.xpath('//select[@id="snow_profile_primary_grain_subshape_' + primaryShape + '"]'));
-            }, 2000, 'subselect for ' + primaryShape + ' not found')
-            .then(function() {
-              driver.findElement(sw.By.xpath('//select[@id="snow_profile_primary_grain_subshape_' + primaryShape + '"]/option[@value="' + primarySubShape + '"]')).click();
-            });
-        }
-        if (secondaryShape !== undefined) {
-          driver.wait(function() {
-            return driver.isElementPresent(sw.By.xpath('//select[@id="snow_profile_secondary_grain_select"]'));
-            },
-            2000, 'select secondary_grain_select not found')
-            .then(function() {
-              driver.findElement(sw.By.xpath('//select[@id="snow_profile_secondary_grain_select"]/option[@value="' + secondaryShape + '"]')).click();
-              });
-          if (secondarySubShape !== undefined) {
-            driver.wait(function() {
-              return driver.isElementPresent(sw.By.xpath('//select[@id="snow_profile_secondary_grain_subshape_' + secondaryShape + '"]'));
-            },
-            2000, 'select secondary_grain_subshape not found')
-            .then(function() {
-              driver.findElement(sw.By.xpath('//select[@id="snow_profile_secondary_grain_subshape_'+ secondaryShape +'"]/option[@value="' + secondarySubShape + '"]')).click();
-              });
-          }
-        }
-      }
+      // Set grain shape
+      setFeaturesShape(sw, driver, shape);
 
-      // Grain size is one number or a range
-      if (sizeMin !== undefined) {
+      // Set grain size
+      setFeaturesSize(sw, driver, size);
 
-        // If there is existing minimum grain size, erase it
-	driver.executeScript('return $("#snow_profile_grain_size_min").val()')
-	  .then(function(val) {
-            if (val.length > 0) {
-              for (i = 0; i < val.length; i++) {
-                cmdStrMin.push(sw.Key.BACK_SPACE);
-              }
-              cmdStrMin.push(sw.Key.NULL);
-          }
-	  });
-
-	// After backspacing over existing contents of input box,
-	// type in the minimum grain size
-        driver.findElement(sw.By.css('#snow_profile_grain_size_min'))
-          .then(function(elmt) {
-            cmdStrMin.push(sizeMin);
-            elmt.sendKeys.apply(elmt, cmdStrMin);
-          });
-      }
-
-      if (sizeMax !== undefined) {
-        // If there is existing maximum grain size, erase it
-	driver.executeScript('return $("#snow_profile_grain_size_max").val()')
-	  .then(function(val) {
-            if (val.length > 0) {
-              for (i = 0; i < val.length; i++) {
-                cmdStrMax.push(sw.Key.BACK_SPACE);
-              }
-              cmdStrMax.push(sw.Key.NULL);
-            }
-	  });
-
-	// After backspacing over existing contents of input box,
-	// type in the maximum grain size
-        driver.findElement(sw.By.css('#snow_profile_grain_size_max'))
-          .then(function(elmt) {
-            cmdStrMax.push(sizeMax);
-            elmt.sendKeys.apply(elmt, cmdStrMax);
-          });
-      }
-
-      // Comment is a string or null
-      if ((comment !== null) && (comment !== undefined)) {
-        setFeaturesComment(sw, driver, comment);
-      }
+      // Set comment
+      setFeaturesComment(sw, driver, comment);
 
       // Click the Done button to save the features
       driver.findElement(sw.By.xpath('//button[.="Done"]')).click();
